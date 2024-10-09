fc6dbcdfc3f3fcad87f6d38399e8d18eb72319a3
Messi002 <rostandmessi2@gmail.com>
2024-10-09 14:30:00 +0100

    added some code

--- chatenv/backend/chat/apps.py
+++ chatenv/backend/chat/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class ChatConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'chat'
+git push --set-upstream origin hotfixesgit push --set-upstream origin hotfixesgit push --set-upstream origin hotfixesgit push --set-upstream origin hotfixes
